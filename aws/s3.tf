c61c7219f61882131eada0c2725e014c215db831
NasuTaiyo <t.nasu.73@arlue.jp>
2024-11-07 03:00:28 +0000

    modify:s3

--- aws/s3.tf
+++ aws/s3.tf
@@ -150,3 +150,86 @@ resource "aws_s3_bucket_lifecycle_configuration" "tfstate" {
   }
 }
 
+# ------------------------------------------------------------#
+#  ssm log
+# ------------------------------------------------------------#
+
+resource "aws_s3_bucket" "ssm_log" {
+  bucket = "${local.PJPrefix}-${local.EnvPrefix}-ssm-log"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "ssm_log" {
+  bucket                  = aws_s3_bucket.ssm_log.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ssm_log" {
+  bucket = aws_s3_bucket.ssm_log.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# ------------------------------------------------------------#
+#  access_log
+# ------------------------------------------------------------#
+
+resource "aws_s3_bucket" "alb_access_log" {
+  bucket = "${local.PJPrefix}-${local.EnvPrefix}-alb-access-log"
+}
+
+resource "aws_s3_bucket_public_access_block" "alb_access_log" {
+  bucket                  = aws_s3_bucket.alb_access_log.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "alb_access_log" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.alb_access_log.arn}/*",
+      "${aws_s3_bucket.alb_access_log.arn}"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::582318560864:root"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_access_log" {
+  bucket = aws_s3_bucket.alb_access_log.id
+  policy = data.aws_iam_policy_document.alb_access_log.json
+}
+
+# ------------------------------------------------------------#
+#  athena result
+# ------------------------------------------------------------#
+
+resource "aws_s3_bucket" "athena_result" {
+  bucket = "${local.PJPrefix}-${local.EnvPrefix}-athna-result"
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_result" {
+  bucket                  = aws_s3_bucket.athena_result.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
