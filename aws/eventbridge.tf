ca95c55cc6e55e53ef4713935f1576a86497e299
NasuTaiyo <t.nasu.73@arlue.jp>
2025-07-10 02:11:39 +0000

    add:異常検知を追加

--- aws/eventbridge.tf
+++ aws/eventbridge.tf
@@ -1,6 +1,11 @@
 # ------------------------------------------------------------#
-#  Common
+#  eventbrige scheduler
 # ------------------------------------------------------------#
+
+## ------------------------------------------------------------#
+##  eventbrige scheduler role
+## ------------------------------------------------------------#
+
 resource "aws_iam_role" "eventbridge_scheduler" {
   assume_role_policy = <<POLICY
 {
@@ -17,7 +22,6 @@ resource "aws_iam_role" "eventbridge_scheduler" {
 }
 POLICY
 
-  managed_policy_arns  = ["${aws_iam_policy.eventbridge_scheduler.arn}"]
   max_session_duration = "3600"
   name                 = "${local.PJPrefix}-${local.EnvPrefix}-eventbridge-scheduler-role"
   path                 = "/"
@@ -58,9 +62,14 @@ resource "aws_iam_policy" "eventbridge_scheduler" {
 POLICY
 }
 
-# ------------------------------------------------------------#
-#  rds
-# ------------------------------------------------------------#
+resource "aws_iam_role_policy_attachment" "eventbridge_scheduler" {
+  role       = aws_iam_role.eventbridge_scheduler.name
+  policy_arn = aws_iam_policy.eventbridge_scheduler.arn
+}
+
+## ------------------------------------------------------------#
+##  rds auto start
+## ------------------------------------------------------------#
 /*
 resource "aws_scheduler_schedule" "rds_start" {
   name       = "${local.PJPrefix}-${local.EnvPrefix}-rds-start"
@@ -85,6 +94,10 @@ resource "aws_scheduler_schedule" "rds_start" {
   }
 }
 
+## ------------------------------------------------------------#
+##  rds auto stop
+## ------------------------------------------------------------#
+
 resource "aws_scheduler_schedule" "rds_stop" {
   name       = "${local.PJPrefix}-${local.EnvPrefix}-rds-stop"
   #group_name = "default"
@@ -108,9 +121,9 @@ resource "aws_scheduler_schedule" "rds_stop" {
   }
 }
 */
-# ------------------------------------------------------------#
-#  ec2
-# ------------------------------------------------------------#
+## ------------------------------------------------------------#
+##  ec2 auto stop
+## ------------------------------------------------------------#
 
 resource "aws_scheduler_schedule" "ec2_stop" {
   name       = "${local.PJPrefix}-${local.EnvPrefix}-ec2-stop"
@@ -135,9 +148,9 @@ resource "aws_scheduler_schedule" "ec2_stop" {
   }
 }
 /*
-# ------------------------------------------------------------#
-#  ecs
-# ------------------------------------------------------------#
+## ------------------------------------------------------------#
+##  ecs auto start
+## ------------------------------------------------------------#
 
 resource "aws_scheduler_schedule" "ecs_start" {
   name       = "${local.PJPrefix}-${local.EnvPrefix}-ecs-start"
@@ -164,6 +177,10 @@ resource "aws_scheduler_schedule" "ecs_start" {
   }
 }
 
+## ------------------------------------------------------------#
+##  ecs auto stop
+## ------------------------------------------------------------#
+
 resource "aws_scheduler_schedule" "ecs_stop" {
   name       = "${local.PJPrefix}-${local.EnvPrefix}-ecs-stop"
   #group_name = "default"
@@ -189,11 +206,314 @@ resource "aws_scheduler_schedule" "ecs_stop" {
   }
 }
 */
+
+## ------------------------------------------------------------#
+##  ecs flask auto start
+## ------------------------------------------------------------#
 /*
+resource "aws_scheduler_schedule" "flask_start" {
+  name       = "${local.PJPrefix}-${local.EnvPrefix}-flask-start"
+  #group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+
+  schedule_expression_timezone = "Asia/Tokyo"
+  schedule_expression          = "cron(00 8 ? * MON-FRI *)"
+
+  target {
+    arn      = "arn:aws:scheduler:::aws-sdk:ecs:updateService"
+    role_arn = "${aws_iam_role.eventbridge_scheduler.arn}"
+
+    retry_policy {
+      maximum_retry_attempts = 3
+    }
+
+    input = jsonencode({
+      Service      = "${aws_ecs_service.flask.name}",
+      Cluster      = "${aws_ecs_cluster.this.id}",
+      DesiredCount = 2
+    })
+  }
+}
+
+## ------------------------------------------------------------#
+##  ecs flask auto stop
+## ------------------------------------------------------------#
+
+resource "aws_scheduler_schedule" "flask_stop" {
+  name       = "${local.PJPrefix}-${local.EnvPrefix}-flask-stop"
+  #group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+  
+
+  schedule_expression_timezone = "Asia/Tokyo"
+  schedule_expression          = "cron(50 11 ? * MON-FRI *)"
+  #state = "ENABLED"
+
+  target {
+    arn      = "arn:aws:scheduler:::aws-sdk:ecs:updateService"
+    role_arn = "${aws_iam_role.eventbridge_scheduler.arn}"
+
+    input = jsonencode({
+      Service      = "${aws_ecs_service.flask.name}",
+      Cluster      = "${aws_ecs_cluster.this.id}",
+      DesiredCount = 0
+    })
+  }
+}
+*/
+
 # ------------------------------------------------------------#
-#  ecs scheduled task role
+#  eventbrige rule
 # ------------------------------------------------------------#
 
+## ------------------------------------------------------------#
+##  eventbridge rule sns target role
+## ------------------------------------------------------------#
+
+resource "aws_iam_role" "eventbridge_rule_sns_target" {
+  assume_role_policy    = data.aws_iam_policy_document.eventbridge_rule_sns_target_assume_role_policy.json
+  max_session_duration  = "3600"
+  name                  = "${local.PJPrefix}-${local.EnvPrefix}-eventbridge-rule-sns-target-role"
+  path                  = "/"
+}
+
+data "aws_iam_policy_document" "eventbridge_rule_sns_target_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "eventbridge_rule_sns_target" {
+  name   = "${local.PJPrefix}-${local.EnvPrefix}-eventbridge-rule-sns-target-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.eventbridge_rule_sns_target.json
+}
+
+data "aws_iam_policy_document" "eventbridge_rule_sns_target" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [
+      "${aws_sns_topic.slack.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_rule_sns_target" {
+  role       = aws_iam_role.eventbridge_rule_sns_target.name
+  policy_arn = aws_iam_policy.eventbridge_rule_sns_target.arn
+}
+
+## ------------------------------------------------------------#
+##  ecs flask abnormal stop detection
+## ------------------------------------------------------------#
+/*
+resource "aws_cloudwatch_log_group" "ecs_flask_abnormal_stop" {
+  name = "/aws/events/${local.PJPrefix}-${local.EnvPrefix}-ecs-test-abnormal-stop"
+
+  tags = {
+    Service = "${local.PJPrefix}-${local.EnvPrefix}-flask"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "ecs_flask_abnormal_stop" {
+  name                = "${local.PJPrefix}-${local.EnvPrefix}-ecs-test-abnormal-stop"
+  event_pattern = <<-EOT
+  {
+    "source": [
+      "aws.ecs"
+    ],
+    "detail-type": [
+      "ECS Task State Change"
+    ],
+    "detail": {
+      "clusterArn": ["${aws_ecs_cluster.this.arn}"],
+      "$or": [{
+        "containers": {
+          "exitCode": [{
+            "anything-but": 0
+          }]
+        }
+      }, {
+        "stoppedReason": [{
+          "anything-but": {
+            "prefix": "Scaling activity initiated by"
+          }
+        }]
+      }],
+      "lastStatus": ["STOPPED"], 
+      "taskDefinitionArn" : [{ "prefix": "${aws_ecs_task_definition.flask.arn_without_revision}"}]
+    }
+  }
+  EOT
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "ecs_flask_abnormal_stop_log" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.ecs_flask_abnormal_stop,
+    aws_cloudwatch_log_group.ecs_flask_abnormal_stop
+  ]
+  
+  rule = "${local.PJPrefix}-${local.EnvPrefix}-ecs-test-abnormal-stop"
+  arn  = aws_cloudwatch_log_group.ecs_flask_abnormal_stop.arn
+
+}
+
+resource "aws_cloudwatch_event_target" "ecs_flask_abnormal_stop_notification" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.ecs_flask_abnormal_stop,
+    aws_cloudwatch_log_group.ecs_flask_abnormal_stop
+  ]
+  
+  rule     = "${local.PJPrefix}-${local.EnvPrefix}-ecs-test-abnormal-stop"
+  arn      = aws_sns_topic.slack.arn
+  role_arn = aws_iam_role.eventbridge_rule_sns_target.arn
+
+}
+*/
+## ------------------------------------------------------------#
+##  rds instance abnormal operation detection
+## ------------------------------------------------------------#
+/*
+resource "aws_cloudwatch_log_group" "rds_instance_abnormal_operation" {
+  name = "/aws/events/${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation"
+
+}
+
+resource "aws_cloudwatch_event_rule" "rds_instance_abnormal_operation" {
+  name                = "${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation"
+  event_pattern = <<-EOT
+  {
+    "source": [
+      "aws.rds"
+    ],
+    "resources": ["${aws_rds_cluster_instance.this.arn}"],
+    "detail.EventID": [
+      "RDS-EVENT-0003",
+      "RDS-EVENT-0006",
+      "RDS-EVENT-0087",
+      "RDS-EVENT-0088",
+      "RDS-EVENT-0178"
+    ]
+  }
+  EOT
+  # "RDS-EVENT-0003", DBインスタンスの削除検知
+  # "RDS-EVENT-0006", DBインスタンスの再起動検知
+  # "RDS-EVENT-0087", DBインスタンスの停止検知
+  # "RDS-EVENT-0088", DBインスタンスの開始検知
+  # "RDS-EVENT-0178"  DBインスタンスのバージョンアップグレード検知
+
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "rds_instance_abnormal_operation_log" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.rds_instance_abnormal_operation,
+    aws_cloudwatch_log_group.rds_instance_abnormal_operation
+  ]
+  
+  target_id = "${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation-log"
+  rule      = "${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation"
+  arn       = aws_cloudwatch_log_group.rds_instance_abnormal_operation.arn
+
+}
+
+resource "aws_cloudwatch_event_target" "rds_instance_abnormal_operation_notification" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.rds_instance_abnormal_operation,
+    aws_cloudwatch_log_group.rds_instance_abnormal_operation
+  ]
+  
+  target_id = "${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation-notification"
+  rule     = "${local.PJPrefix}-${local.EnvPrefix}-rds-instance-abnormal-operation"
+  arn      = aws_sns_topic.slack.arn
+  role_arn = aws_iam_role.eventbridge_rule_sns_target.arn
+
+}
+*/
+## ------------------------------------------------------------#
+##  rds cluster abnormal operation detection
+## ------------------------------------------------------------#
+
+resource "aws_cloudwatch_log_group" "rds_cluster_abnormal_operation" {
+  name = "/aws/events/${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation"
+
+}
+
+resource "aws_cloudwatch_event_rule" "rds_cluster_abnormal_operation" {
+  name                = "${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation"
+  event_pattern = <<-EOT
+  {
+    "source": [
+      "aws.rds"
+    ],
+    "resources": ["${aws_rds_cluster.this.arn}"],
+    "detail.EventID": [
+      "RDS-EVENT-0150", 
+      "RDS-EVENT-0151", 
+      "RDS-EVENT-0171", 
+      "RDS-EVENT-0173"
+    ]
+  }
+  EOT
+  # "RDS-EVENT-0150", DBクラスターの停止検知
+  # "RDS-EVENT-0151", DBクラスターの開始検知
+  # "RDS-EVENT-0171", DBクラスターの削除検知
+  # "RDS-EVENT-0173"  DBクラスターのバージョンアップグレード検知
+  state               = "ENABLED"
+}
+
+resource "aws_cloudwatch_event_target" "rds_cluster_abnormal_operation_log" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.rds_cluster_abnormal_operation,
+    aws_cloudwatch_log_group.rds_cluster_abnormal_operation
+  ]
+  
+  target_id = "${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation-log"
+  rule      = "${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation"
+  arn       = aws_cloudwatch_log_group.rds_cluster_abnormal_operation.arn
+
+}
+
+resource "aws_cloudwatch_event_target" "rds_cluster_abnormal_operation_notification" {
+  depends_on     = [
+    aws_cloudwatch_event_rule.rds_cluster_abnormal_operation,
+    aws_cloudwatch_log_group.rds_cluster_abnormal_operation
+  ]
+  
+  target_id = "${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation-notification"
+  rule      = "${local.PJPrefix}-${local.EnvPrefix}-rds-cluster-abnormal-operation"
+  arn       = aws_sns_topic.slack.arn
+  role_arn  = aws_iam_role.eventbridge_rule_sns_target.arn
+
+}
+
+# ------------------------------------------------------------#
+#  ecs scheduled task 
+# ------------------------------------------------------------#
+
+## ------------------------------------------------------------#
+##  ecs scheduled task role
+## ------------------------------------------------------------#
+/*
 resource "aws_iam_role" "ecs_scheduled_task" {
   assume_role_policy    = data.aws_iam_policy_document.ecs_scheduled_task_assume_role_policy.json
   max_session_duration  = "3600"
@@ -251,7 +571,7 @@ resource "aws_iam_role_policy_attachment" "ecs_scheduled_task" {
 }
 
 # ------------------------------------------------------------#
-#  ecs scheduled task
+#  ecs scheduled task rule
 # ------------------------------------------------------------#
 
 resource "aws_cloudwatch_event_rule" "ecs_scheduled_task" {
