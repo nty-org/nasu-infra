7487fbcb5f450da920f05dc665d44efdc2cf0218
NasuTaiyo <t.nasu.73@arlue.jp>
2025-07-26 15:27:16 +0000

    modify:ecrファイルの修正

--- monolith/terraform/ecr.tf
+++ monolith/terraform/ecr.tf
@@ -1,4 +1,7 @@
-resource "aws_ecr_repository" "tf-bg-rp05" {
+# ------------------------------------------------------------#
+#  app
+# ------------------------------------------------------------#
+resource "aws_ecr_repository" "app" {
   encryption_configuration {
     encryption_type = "AES256"
   }
@@ -8,7 +11,7 @@ resource "aws_ecr_repository" "tf-bg-rp05" {
   }
 
   image_tag_mutability = "MUTABLE"
-  name                 = "bg-rp05"
+  name                 = "${local.PJPrefix}/${local.EnvPrefix}/app"
 }
 
 # ------------------------------------------------------------#
@@ -29,70 +32,6 @@ resource "aws_ecr_repository" "api" {
 
   tags = {
     Service = "${local.PJPrefix}-${local.EnvPrefix}-api"
-  }
-}
-*/
-# ------------------------------------------------------------#
-#  django
-# ------------------------------------------------------------#
-/*
-resource "aws_ecr_repository" "django" {
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
-  image_scanning_configuration {
-    scan_on_push = "false"
-  }
-
-  image_tag_mutability = "MUTABLE"
-  name                 = "${local.PJPrefix}/${local.EnvPrefix}/django"
-
-  tags = {
-    Service = "${local.PJPrefix}-${local.EnvPrefix}-django"
-  }
-}
-*/
-/*
-# ------------------------------------------------------------#
-#  sync
-# ------------------------------------------------------------#
-
-resource "aws_ecr_repository" "sync" {
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
-  image_scanning_configuration {
-    scan_on_push = "false"
-  }
-
-  image_tag_mutability = "MUTABLE"
-  name                 = "${local.PJPrefix}/${local.EnvPrefix}/sync"
-
-  tags = {
-    Service = "${local.PJPrefix}-${local.EnvPrefix}-sync"
-  }
-}
-
-# ------------------------------------------------------------#
-#  web
-# ------------------------------------------------------------#
-
-resource "aws_ecr_repository" "web" {
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
-  image_scanning_configuration {
-    scan_on_push = "false"
-  }
-
-  image_tag_mutability = "MUTABLE"
-  name                 = "${local.PJPrefix}/${local.EnvPrefix}/web"
-
-  tags = {
-    Service = "${local.PJPrefix}-${local.EnvPrefix}-web"
   }
 }
 */
