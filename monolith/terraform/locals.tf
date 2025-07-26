fbec6d86a17d4b8b347eac868da2f56c582e6677
NasuTaiyo <t.nasu.73@arlue.jp>
2025-07-26 17:10:37 +0000

    modify:localsの修正

--- monolith/terraform/locals.tf
+++ monolith/terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  PJPrefix   = "nasu"
-  EnvPrefix  = "prod"
+  PJPrefix  = "nasu"
+  EnvPrefix = "prod"
 
   account_id = "267751904634"
-  
+
   zone_name = "sunasuna.net"
 
   vpc_cidr = "10.1.0.0/16"
@@ -29,12 +29,10 @@ locals {
 
   slack_workspace_id = "T02KGURL8BG"
 
-  slack_channel_id   = "C05H3HTMLSY"
+  slack_channel_id = "C05H3HTMLSY"
 
   slack_channel_name = "times_nasu"
 
   vercel_team_slug = "times_nasu"
-  
-  domain_name = "nasu.com"
-  
+
 }
