05d33417e1d1b06a38c2ace377818a52f342ce6a
NasuTaiyo <t.nasu.73@arlue.jp>
2025-07-26 06:49:46 +0000

    modify:chatbot用ファイルの修正

--- monolith/terraform/chatbot.tf
+++ monolith/terraform/chatbot.tf
@@ -1,32 +1,22 @@
 # ------------------------------------------------------------#
-#  slack
+#  role
 # ------------------------------------------------------------#
 
-resource "awscc_chatbot_slack_channel_configuration" "this" {
-
-  configuration_name = "${local.slack_channel_name}"
-
-  slack_workspace_id = "${local.slack_workspace_id}"
-  slack_channel_id = "${local.slack_channel_id}"
-
-  iam_role_arn = aws_iam_role.chatbot_slack.arn
-
-  sns_topic_arns = [aws_sns_topic.slack.arn]
-
-  guardrail_policies = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-}
+## ------------------------------------------------------------#
+##  slack
+## ------------------------------------------------------------#
 
 resource "aws_iam_role" "chatbot_slack" {
-  assume_role_policy    = data.aws_iam_policy_document.chatbot_slack_assume_role_policy.json
-  max_session_duration  = "3600"
-  name                  = "${local.PJPrefix}-${local.EnvPrefix}-chatbot-slack-role"
-  path                  = "/"
+  assume_role_policy   = data.aws_iam_policy_document.chatbot_slack_assume_role_policy.json
+  max_session_duration = "3600"
+  name                 = "${local.PJPrefix}-${local.EnvPrefix}-chatbot-slack-role"
+  path                 = "/service-role/"
 
 }
 
 data "aws_iam_policy_document" "chatbot_slack_assume_role_policy" {
   statement {
-    effect  = "Allow"
+    effect = "Allow"
     actions = [
       "sts:AssumeRole"
     ]
@@ -69,4 +59,26 @@ resource "aws_iam_role_policy_attachment" "chatbot_slack" {
 resource "aws_iam_role_policy_attachment" "chatbot_slack_AWSResourceExplorerReadOnlyAccess" {
   role       = aws_iam_role.chatbot_slack.name
   policy_arn = "arn:aws:iam::aws:policy/AWSResourceExplorerReadOnlyAccess"
+}
+
+# ------------------------------------------------------------#
+#  client
+# ------------------------------------------------------------#
+
+## ------------------------------------------------------------#
+##  slack
+## ------------------------------------------------------------#
+
+resource "awscc_chatbot_slack_channel_configuration" "slack" {
+
+  configuration_name = local.slack_channel_name
+
+  slack_workspace_id = local.slack_workspace_id
+  slack_channel_id   = local.slack_channel_id
+
+  iam_role_arn = aws_iam_role.chatbot_slack.arn
+
+  sns_topic_arns = [aws_sns_topic.slack.arn]
+
+  guardrail_policies = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
 }
