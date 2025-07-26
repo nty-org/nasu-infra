61c1f57291439ba2cbabaab10fc828a2ef42ed72
NasuTaiyo <t.nasu.73@arlue.jp>
2025-07-26 16:00:21 +0000

    kmsファイルの修正

--- monolith/terraform/kms.tf
+++ monolith/terraform/kms.tf
@@ -1,6 +1,10 @@
 # ------------------------------------------------------------#
-#  cloudtrail
+#  cmk
 # ------------------------------------------------------------#
+
+## ------------------------------------------------------------#
+##  cloudtrail
+## ------------------------------------------------------------#
 
 resource "aws_kms_key" "cloudtrail" {
   key_usage                = "ENCRYPT_DECRYPT"
@@ -26,31 +30,31 @@ data "aws_iam_policy_document" "kms_cloudtrail" {
     sid       = "Enable IAM user permissions"
     actions   = ["kms:*"]
     resources = ["*"]
-    
+
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${local.account_id}:root"]
     }
-    
+
   }
 
   statement {
     sid       = "Allow CloudTrail to encrypt logs"
     actions   = ["kms:GenerateDataKey*"]
     resources = ["*"]
-    
+
     principals {
       type        = "Service"
       identifiers = ["cloudtrail.amazonaws.com"]
     }
-    
+
     #${local.PJPrefix}-${local.EnvPrefix}-s3 証跡へ権限を許可
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
       values   = ["arn:aws:cloudtrail:ap-northeast-1:${local.account_id}:trail/${local.PJPrefix}-${local.EnvPrefix}-s3"]
     }
-    
+
     #${local.PJPrefix}-${local.EnvPrefix}-management-event 証跡へ権限を許可
     condition {
       test     = "StringEquals"
@@ -63,40 +67,40 @@ data "aws_iam_policy_document" "kms_cloudtrail" {
       variable = "kms:EncryptionContext:aws:cloudtrail:arn"
       values   = ["arn:aws:cloudtrail:*:${local.account_id}:trail/*"]
     }
-    
+
   }
 
   statement {
     sid       = "Allow CloudTrail to describe key"
     actions   = ["kms:DescribeKey"]
     resources = ["*"]
-    
+
     principals {
       type        = "Service"
       identifiers = ["cloudtrail.amazonaws.com"]
     }
-    
+
   }
 
   statement {
-    sid       = "Allow principals in the account to decrypt log files"
-    actions   = [
+    sid = "Allow principals in the account to decrypt log files"
+    actions = [
       "kms:Decrypt",
       "kms:ReEncryptFrom"
     ]
     resources = ["*"]
-    
+
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
-    
+
     condition {
       test     = "StringEquals"
       variable = "kms:CallerAccount"
       values   = [local.account_id]
     }
-    
+
     condition {
       test     = "StringLike"
       variable = "kms:EncryptionContext:aws:cloudtrail:arn"
@@ -106,23 +110,23 @@ data "aws_iam_policy_document" "kms_cloudtrail" {
   }
 
   statement {
-    sid       = "Allow alias creation during setup"
-    actions   = [
+    sid = "Allow alias creation during setup"
+    actions = [
       "kms:CreateAlias"
     ]
     resources = [aws_kms_key.cloudtrail.arn]
-    
+
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
-    
+
     condition {
       test     = "StringEquals"
       variable = "kms:CallerAccount"
       values   = [local.account_id]
     }
-    
+
     condition {
       test     = "StringEquals"
       variable = "kms:ViaService"
@@ -132,24 +136,24 @@ data "aws_iam_policy_document" "kms_cloudtrail" {
   }
 
   statement {
-    sid       = "Enable cross account log decryption"
-    actions   = [
+    sid = "Enable cross account log decryption"
+    actions = [
       "kms:Decrypt",
       "kms:ReEncryptFrom"
     ]
     resources = ["*"]
-    
+
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
-    
+
     condition {
       test     = "StringEquals"
       variable = "kms:CallerAccount"
       values   = [local.account_id]
     }
-    
+
     condition {
       test     = "StringLike"
       variable = "kms:EncryptionContext:aws:cloudtrail:arn"
@@ -157,5 +161,5 @@ data "aws_iam_policy_document" "kms_cloudtrail" {
     }
 
   }
-  
+
 }
