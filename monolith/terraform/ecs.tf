10e21691230abf35353f98cd94757e909a1a8b40
NasuTaiyo <t.nasu.73@arlue.jp>
2025-08-27 02:21:44 +0000

    add:ecsタスクロール　タスク実行ロールの分離

--- monolith/terraform/ecs.tf
+++ monolith/terraform/ecs.tf
@@ -2,6 +2,113 @@
 #  role
 # ------------------------------------------------------------#
 
+## ------------------------------------------------------------#
+##  ecs role
+## ------------------------------------------------------------#
+
+resource "aws_iam_role" "ecs_task" {
+  assume_role_policy   = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+  max_session_duration = "3600"
+  name                 = "${local.PJPrefix}-${local.EnvPrefix}-ecs-task-role"
+  path                 = "/service-role/"
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:ecs:ap-northeast-1:${local.account_id}:*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.account_id]
+    }
+
+  }
+}
+
+resource "aws_iam_policy" "ecs_task" {
+  name   = "${local.PJPrefix}-${local.EnvPrefix}-ecs-task-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.ecs_task.json
+
+}
+
+data "aws_iam_policy_document" "ecs_task" {
+
+  # secretsmangerからの環境変数取得用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  # ecs exec用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  # ecs execログ保存用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = aws_iam_policy.ecs_task.arn
+}
+
+## ------------------------------------------------------------#
+##  ecs task role
+## ------------------------------------------------------------#
+/*
 resource "aws_iam_role" "ecs_task" {
   assume_role_policy   = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
   max_session_duration = "3600"
@@ -120,6 +227,88 @@ resource "aws_iam_role_policy_attachment" "ecs_task" {
   role       = aws_iam_role.ecs_task.name
   policy_arn = aws_iam_policy.ecs_task.arn
 }
+*/
+## ------------------------------------------------------------#
+##  ecs task exec role
+## ------------------------------------------------------------#
+
+resource "aws_iam_role" "ecs_task_exec" {
+  assume_role_policy   = data.aws_iam_policy_document.ecs_task_exec_assume_role_policy.json
+  max_session_duration = "3600"
+  name                 = "${local.PJPrefix}-${local.EnvPrefix}-ecs-task-exec-role"
+  path                 = "/service-role/"
+}
+
+data "aws_iam_policy_document" "ecs_task_exec_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+  }
+}
+
+resource "aws_iam_policy" "ecs_task_exec" {
+  name   = "${local.PJPrefix}-${local.EnvPrefix}-ecs-exec-task-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.ecs_task_exec.json
+
+}
+
+data "aws_iam_policy_document" "ecs_task_exec" {
+
+  # ecrからのイメージ取得用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  # ssm secretsmanagerからの値参照用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters",
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  # cloudwatchへのログ保存用権限
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = aws_iam_policy.ecs_task_exec.arn
+}
+
 
 # ------------------------------------------------------------#
 #  cluster
@@ -264,7 +453,7 @@ resource "aws_ecs_service" "api" {
 ## ------------------------------------------------------------#
 ##  app
 ## ------------------------------------------------------------#
-
+/*
 resource "aws_cloudwatch_log_group" "app" {
   name = "/ecs/${local.PJPrefix}-${local.EnvPrefix}-app"
 
@@ -281,7 +470,7 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions = jsonencode([
     {
       name   = "app"
-      image  = "${local.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/bg-rp05:latest"
+      image  = "${aws_ecr_repository.app.repository_url}:latest"
       cpu    = 256
       memory = 512
       logConfiguration = {
@@ -317,7 +506,7 @@ resource "aws_ecs_task_definition" "app" {
   ])
   cpu                      = "1024"
   memory                   = "2048"
-  execution_role_arn       = aws_iam_role.ecs_task.arn
+  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
   family                   = "${local.PJPrefix}-${local.EnvPrefix}-app-fargate-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -327,7 +516,7 @@ resource "aws_ecs_task_definition" "app" {
     Service = "${local.PJPrefix}-${local.EnvPrefix}-app"
   }
 }
-/*
+
 resource "aws_ecs_service" "app" {
   capacity_provider_strategy {
     base              = "0"
